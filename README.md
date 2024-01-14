8b3eb00147017c709fa8f14e95919ca804345f35
Amit Yadav <amit.yadav.iitr@gmail.com>
2024-01-14 10:29:46 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ONNX-Inference-C
-Using ONNX to run inference in C
+# ONNX Inference C
+
+Using ONNX runtime to run inference on a ONNX mdoel in C.
+
+![ONNX in C](./assets/onnx_in_c.gif)
+
+## Requirements
+
+- C Compiler
+- [Task](https://taskfile.dev) or user-defined build system
+- Raylib
+- ONNX Runtime
+- Conda
+
+I have provided a `Taskfile` to help setup the requirements on macOS quickly with: `task setup`
+
+## Usage
+
+- Install dependencies and setup conda environment: `task setup`
+- Train the model and convert it to ONNX: `task train`
+- Build and run the app: `task`
---
+++ train.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import keras
+
+
+def train():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    x_train = x_train / 255.
+    x_test = x_test / 255.
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28), name="model_input"),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(10, activation="softmax", name="model_output")
+    ])
+
+    model.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"]
+    )
+
+    _ = model.fit(
+        x_train, y_train,
+        validation_data=(x_test, y_test),
+        batch_size=64,
+        epochs=5
+    )
+
+    tf.saved_model.save(model, "saved_model")
+
+
+if __name__ == "__main__":
+    train()
